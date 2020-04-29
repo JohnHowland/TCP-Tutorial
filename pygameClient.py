28eaedfb5c99f2638ecfdb6d0888d612ba5ec088
John Howland <>
2020-04-29 13:50:05 -0400

    fixed small error in Click to Play screen

--- pygameClient.py
+++ pygameClient.py
@@ -153,14 +153,13 @@ def menu_screen():
         clock.tick(60)
         win.fill((128, 128, 128))
         font = pygame.font.SysFont("comicsans", 60)
-        text = font.render("Click to Play!", (255, 0, 0))
+        text = font.render("Click to Play!", 1, (255, 0, 0))
         win.blit(text, (100,200))
         pygame.display.update()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit():
-                run = False
+                pygame.quit()
                 run = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 run = False
